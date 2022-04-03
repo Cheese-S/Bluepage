3d028ce676fcae765f671751af11aacfe6e5fe14
Cheese-S <yzcalex@icloud.com>
2022-04-03 11:29:38 -0400

    Mongodb Atlas when running in production

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,14 +2,20 @@ const env = require('../env.config')
 const mongoose = require('mongoose')
 const { logger } = require('bs-logger')
 
+
+
 mongoose
     .connect(env.DB_CONNECT)
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
+mongoose.connection.on('connect', function () {
+    logger(`Database connected sucessfully with route ${env.DB_CONNECT}`)
+})
+
 const db = mongoose.connection
 
-logger(`Database connected sucessfully with route ${env.DB_CONNECT}`);
+
 
 module.exports = db
