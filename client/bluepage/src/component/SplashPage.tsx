7b6e72459460b7f6fcd3e78f07c27d09d0cb04c6
hongcheng li <lili09111234@gmail.com>
2022-04-07 21:33:08 -0400

    at least it is loading

---
+++ client/bluepage/src/component/SplashPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function SplashPage(){
+
+    return (
+        <body>
+            <div id="splash-screen">
+                BlUE<br />
+                PAGE
+            </div>
+            <div>
+                THIS IS SO UGGGGGGGGGGGGGGGGGLY
+            </div>
+        </body>
+    )
+}
