4043b2e0a73b037e303fbc14d8cd709ee3bf3e41
hongcheng li <lili09111234@gmail.com>
2022-04-08 17:57:04 -0400

    login and register and splash page

--- client/bluepage/src/App.tsx
+++ client/bluepage/src/App.tsx
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SplashPage from './component/SplashPage'
+import LogInPage from './component/LogInPage';
+import RegisterPage from './component/RegisterPage';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/"  element={<SplashPage/>} />
+      <Route path="/login"  element={<LogInPage/>} />
+      <Route path="/signup"  element={<RegisterPage/>} />
     </Routes>
     </BrowserRouter>
   );
--- client/bluepage/src/component/SplashPage.tsx
+++ client/bluepage/src/component/SplashPage.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import { Typography } from '@mui/material';
+
+
 
 export default function SplashPage(){
 
     return (
         <body>
-            <div id="splash-screen">
-                BlUE<br />
-                PAGE
-            </div>
-            <div>
-                THIS IS SO UGGGGGGGGGGGGGGGGGLY
-            </div>
+            <Box   height="50%" sx={{ minHeight:'200',overflow:'hidden' }}>
+                <Typography variant="h1" align='center' sx={{color:'white', variant:'body1',height: '100%', pt: '10%'}}>BLUE PAGE</Typography>
+            </Box>
+            <Divider> </Divider>
+            <Box  width="100%" height="40%" sx={{  display: 'flex' }}>
+                <Box width="50%" sx={{borderRight: 1 ,borderColor:"#3d63d4"}} >
+                    <Button variant='contained' sx={{backgroundColor:'#5227cc', pl:'10%',pr:'10%',pt:'2%',pb:'2%', ml:'30%',mt:'15%'}}><Typography variant="h6" align='center' sx={{color:'white', variant:'body1'}}>COMIC</Typography></Button>
+                </Box>
+                <Box width="50%">
+                    <Button variant='contained'sx={{backgroundColor:'#d52941',pl:'10%',pr:'10%',pt:'2%',pb:'2%', ml:'30%',mt:'15%'}}><Typography variant="h6" align='center' sx={{color:'white', variant:'body1'}}>STORY</Typography></Button>
+                </Box>
+            </Box>
         </body>
-    )
+    );
 }
