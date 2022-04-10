7960e8580f7e89169369962b2d7a092fe3df6c8b
Cheese-S <yzcalex@icloud.com>
2022-04-10 13:48:58 -0400

    fixed homescreen

--- client/bluepage/src/component/HomePage.tsx
+++ client/bluepage/src/component/HomePage.tsx
@@ -5,38 +5,76 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import { ButtonAppBar } from './NavBar';
-import { ProfileContentCard } from '../subcomponents/ProfileContentCard';
-import { ProfileSubcontentCard } from '../subcomponents/ProfileSubcontentCard';
+import { ViewCard } from './ViewCard'
+import Grid from '@material-ui/core/Grid/Grid';
 
 export const HomePage: React.FC = () => {
     return (
-        <Box style = {{alignItems: 'center', justifyContent: 'center'}}>
-            <ButtonAppBar/>
-            <Link color = 'inherit' href="/home/test" underline = "hover">
-                        <Typography style={{ fontWeight: 'bold', fontSize: '24px', marginLeft: '16px', marginTop: '10px' }}>Most Recent</Typography>                        
-                        </Link>
-            <Box style={{ display: 'flex', flexDirection: 'row', margin: '16px' }}>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                        </Box>
-                        <Link color = 'inherit' href="/home/test" underline = "hover">
-                        <Typography style={{ fontWeight: 'bold', fontSize: '24px', marginLeft: '16px', marginTop: '10px' }}>Trending</Typography>                        
-                        </Link>
-                        <Box style={{ display: 'flex', flexDirection: 'row', margin: '16px' }}>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                        </Box>
-                        <Link color = 'inherit' href="/home/test" underline = "hover">
-                        <Typography style={{ fontWeight: 'bold', fontSize: '24px', marginLeft: '16px', marginTop: '10px' }}>From Your Following</Typography>                        
-                        </Link>
-                        <Box style={{ display: 'flex', flexDirection: 'row', margin: '16px' }}>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                            <ProfileContentCard/>
-                        </Box>
+        <Box style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <ButtonAppBar />
+            <Link color='inherit' href="/home/test" underline="hover">
+                <Typography style={{ fontWeight: 'bold', fontSize: '24px', marginLeft: '16px', marginTop: '10px' }}>Most Recent</Typography>
+            </Link>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+            </Grid>
+            <Link color='inherit' href="/home/test" underline="hover">
+                <Typography style={{ fontWeight: 'bold', fontSize: '24px', marginLeft: '16px', marginTop: '10px' }}>Trending</Typography>
+            </Link>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+            </Grid>
+            <Link color='inherit' href="/home/test" underline="hover">
+                <Typography style={{ fontWeight: 'bold', fontSize: '24px', marginLeft: '16px', marginTop: '10px' }}>From Your Following</Typography>
+            </Link>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+                <Grid item xs={3}>
+                    <ViewCard />
+                </Grid >
+
+            </Grid>
         </Box>
-    )}
+    )
+}
