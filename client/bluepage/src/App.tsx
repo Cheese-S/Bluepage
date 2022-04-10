47d8e5db5d880a63b712be7163cb75091e52ecde
Josh-Ramen <joshuaprajan@gmail.com>
2022-04-09 21:53:15 -0400

    Fixed bad imports.

--- client/bluepage/src/App.tsx
+++ client/bluepage/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SplashPage from './component/SplashPage'
-import LogInPage from './component/LogInPage';
-import RegisterPage from './component/RegisterPage';
-import NavBar from './component/NavBar'
+import { SplashPage } from './component/SplashPage'
+import { LogInPage } from './component/LogInPage';
+import { RegisterPage } from './component/RegisterPage';
+import { ButtonAppBar } from './component/NavBar'
 import ListPage from './component/ListPage';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Route path="/"  element={<SplashPage/>} />
       <Route path="/login"  element={<LogInPage/>} />
       <Route path="/signup"  element={<RegisterPage/>} />
-      <Route path="/nav/test" element={<NavBar/>} />
+      <Route path="/nav/test" element={<ButtonAppBar/>} />
       <Route path="/list/test" element={<ListPage/>} />
     </Routes>
     </BrowserRouter>
--- client/bluepage/src/component/ViewCard.tsx
+++ client/bluepage/src/component/ViewCard.tsx
@@ -4,8 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import Chip from '@mui/material/Chip'
-import * as moment from 'moment'
+import Chip from '@mui/material/Chip';
 
 const cardData = {
     title: "To Kill a Mockingbird",
