7196bf5396355000a4dd6f88cc0357f489e52b99
Josh-Ramen <joshuaprajan@gmail.com>
2022-04-21 23:23:29 -0400

    Added color picking for drawing in edit page

--- client/bluepage/src/component/EditPage.js
+++ client/bluepage/src/component/EditPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react';
-import { Box, TextField, Button } from '@mui/material/';
 import { Stage, Layer, Line } from 'react-konva';
+import { ChromePicker } from 'react-color';
+import { Box, TextField, Button } from '@mui/material/';
+
 import PanToolOutlinedIcon from '@mui/icons-material/PanToolOutlined';
 import ModeOutlinedIcon from '@mui/icons-material/ModeOutlined';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -12,6 +14,8 @@ import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
 export default function EditPage() {
   const [tool, setTool] = useState('pen');
   const [lines, setLines] = useState([]);
+  const [color, setColor] = useState('#000000')
+  const [showColorPicker, setShowColorPicker] = useState(false);
   const isDrawing = useRef(false);
 
   const handleMouseDown = (e) => {
@@ -30,6 +34,7 @@ export default function EditPage() {
     let lastLine = lines[lines.length - 1];
     // add point
     lastLine.points = lastLine.points.concat([point.x, point.y]);
+    lastLine.color = color;
 
     // replace last
     lines.splice(lines.length - 1, 1, lastLine);
@@ -67,7 +72,7 @@ export default function EditPage() {
                   <Line
                     key={i}
                     points={line.points}
-                    stroke="#df4b26"
+                    stroke={line.color}
                     strokeWidth={5}
                     tension={0.5}
                     lineCap="round"
@@ -80,7 +85,11 @@ export default function EditPage() {
             </Stage>
           </Box>
         </Box>
-        <Box style={{ width: '15%' }}/>
+        <Box style={{ width: '15%', display: 'flex', flexDirection: 'column-reverse' }}>
+          {showColorPicker ?
+            <ChromePicker color={color} onChange={(color) => setColor(color.hex)} style={{ paddingBottom: '10px' }}/>
+            : null}
+        </Box>
         <Box style={{ display: 'flex', flexDirection: 'column', width: '5%', height: '100%', backgroundColor: '#9dc3ff', alignItems: 'center', justifyContent: 'space-around' }}>
           <PanToolOutlinedIcon style={{ fontSize: '40px' }}/>
           <ModeOutlinedIcon style={{ fontSize: '40px' }}/>
@@ -88,7 +97,7 @@ export default function EditPage() {
           <InterestsOutlinedIcon style={{ fontSize: '40px' }}/>
           <CompareArrowsOutlinedIcon style={{ fontSize: '40px' }}/>
           <TextIncreaseOutlinedIcon style={{ fontSize: '40px' }}/>
-          <ColorLensOutlinedIcon style={{ fontSize: '40px' }}/>
+          <ColorLensOutlinedIcon onMouseDown={() => setShowColorPicker(!showColorPicker)} style={{ fontSize: '40px', color: showColorPicker ? color : '#000000', cursor: 'pointer' }}/>
         </Box>
       </Box>
     </>
