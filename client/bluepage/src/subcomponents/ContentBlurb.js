d80a672a32c001fd03c253fd2af29e2b61c09046
Josh-Ramen <joshuaprajan@gmail.com>
2022-05-09 01:04:35 -0400

    Implemented voting for comics and stories.

--- client/bluepage/src/subcomponents/ContentBlurb.js
+++ client/bluepage/src/subcomponents/ContentBlurb.js
@@ -1,27 +1,98 @@
 import React, { useEffect ,useState} from 'react';
 import { Typography, Button, Link, Box , Chip} from '@mui/material/';
+import { getContentById, voteOnContent } from '../api/api';
+import { CONTENT_TYPE, VOTE_STATE_TYPE } from '../constant';
+import { useNavigate } from 'react-router-dom';
+import { userStore } from '../store/UserStore';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
-import { getContentById, getSubcontentByID } from '../api/api';
-import { useNavigate } from 'react-router-dom'; 
 
 
 export default function ContentBlurb(props) {
     const history = useNavigate();
     const { id, type, subtype } = props;
+
+    const loggedIn = userStore(state => state.isLoggedIn);
+    const likedContent = (type === CONTENT_TYPE.COMIC) ? userStore(state => state.likedComics) : userStore(state => state.likedStories);
+    const setLikedContent = (type === CONTENT_TYPE.COMIC) ? userStore(state => state.setLikedComics) : userStore(state => state.setLikedStories);
+    const dislikedContent = (type === CONTENT_TYPE.COMIC) ? userStore(state => state.dislikedComics) : userStore(state => state.dislikedStories);
+    const setDislikedContent = (type === CONTENT_TYPE.COMIC) ? userStore(state => state.setDislikedComics) : userStore(state => state.setDislikedStories);
+
     const [title, settitle] = useState(null);
     const [views, setviews] = useState(0);
+    const [description, setdescription] = useState(null);
+    const [tag, settag] = useState(null);
     const [followers, setfollowers] = useState(0);
+
     const [author, setauthor] = useState(null);
     const [authoridlink, setauthorid] = useState(null);
-    const [tag, settag] = useState(null);
-    const [like, setlike] = useState(0);
-    const [dislike, setdislike] = useState(0);
-    const [description, setdescription] = useState(null);
+
+    const [vote, setVote] = useState(VOTE_STATE_TYPE.NEUTRAL);
+    const [likes, setLikes] = useState(0);
+    const [dislikes, setDislikes] = useState(0);
+    
     const handletoauthor= () =>{
         var his = `/profile/${authoridlink}`
         history(his);
-    }
+    };
+
+    /**
+     * Handles changing the like/dislike front-end and then send corresponding request
+     * @param {Number} newVote from the imported VOTE_STATE_TYPE constant
+     */
+    const handleChangeVote = async (newVote) => {
+        if (!loggedIn) return;
+
+        // Update local store and displayed vote counts
+        const tempLiked = [...likedContent];
+        const tempDisliked = [...dislikedContent];
+        if (newVote === VOTE_STATE_TYPE.NEUTRAL) {
+            // Check old vote - will have to be removed
+            if (vote === VOTE_STATE_TYPE.LIKE) {
+                const removeIndex = tempLiked.indexOf(id);
+                tempLiked.splice(removeIndex, 1);
+                setLikedContent(tempLiked);
+                setLikes(likes - 1);
+            } else {
+                const removeIndex = tempDisliked.indexOf(id);
+                tempDisliked.splice(removeIndex, 1);
+                setDislikedContent(tempDisliked);
+                setDislikes(dislikes - 1);
+            }
+        } else if (newVote === VOTE_STATE_TYPE.LIKE) {
+            // Add to list of liked comics - remove from disliked if needed
+            tempLiked.push(id);
+            setLikedContent(tempLiked);
+            setLikes(likes + 1);
+
+            if (vote === VOTE_STATE_TYPE.DISLIKE) {
+                const removeIndex = tempDisliked.indexOf(id);
+                if (removeIndex > -1) tempDisliked.splice(removeIndex, 1);
+                setDislikedContent(tempDisliked);
+                setDislikes(dislikes - 1);
+            }
+        } else {
+            // Add to list of disliked comics - remove from liked if needed
+            tempDisliked.push(id);
+            setDislikedContent(tempDisliked);
+            setDislikes(dislikes + 1);
+
+            if (vote === VOTE_STATE_TYPE.LIKE) {
+                const removeIndex = tempLiked.indexOf(id);
+                if (removeIndex > -1) tempLiked.splice(removeIndex, 1);
+                setLikedContent(tempLiked);
+                setLikes(likes - 1);
+            }
+        }
+    
+        // Update server
+        await voteOnContent(type, id, vote, newVote);
+
+        // Update front-end
+        setVote(newVote);
+    };
 
     useEffect(() => {
         const getcontent = async () =>{
@@ -34,8 +105,17 @@ export default function ContentBlurb(props) {
                 setauthorid(res.data.content.author.id);
                 settag(res.data.content.tags);
                 setdescription(res.data.content.description);
-                setlike(res.data.content.likes);
-                setdislike(res.data.content.dislikes);
+                setLikes(res.data.content.likes);
+                setDislikes(res.data.content.dislikes);
+
+                if (loggedIn) {
+                    // Process the current user's vote, if any
+                    if (likedContent.indexOf(id) > -1) {
+                        setVote(VOTE_STATE_TYPE.LIKE);
+                    } else if (dislikedContent.indexOf(id) > -1) {
+                        setVote(VOTE_STATE_TYPE.DISLIKE);
+                    }
+                } 
             } 
             catch(err){
                 console.log(err);
@@ -76,10 +156,17 @@ export default function ContentBlurb(props) {
                     <Button variant='contained'>Follow</Button>
                 </Box>
                 <Box sx={{ display: 'flex', flexDirection: 'row-reverse', alignItems: 'center', width: '30%', marginRight: '20px' }}>
-                    <Typography style={{ fontWeight: 'bold' }}>{like}</Typography>
-                    <ThumbDownOffAltIcon sx={{ fontSize: '40px' }}></ThumbDownOffAltIcon>
-                    <Typography style={{ fontWeight: 'bold', marginRight: '20px' }}>{dislike}</Typography>
-                    <ThumbUpOffAltIcon sx={{ fontSize: '40px' }}></ThumbUpOffAltIcon>
+                    <Typography style={{ fontWeight: 'bold' }}>{dislikes}</Typography>
+                    {vote === VOTE_STATE_TYPE.DISLIKE ?
+                        <ThumbDownIcon onClick={() => handleChangeVote(VOTE_STATE_TYPE.NEUTRAL)} sx={{ fontSize: '40px', cursor: 'pointer' }} />
+                        :
+                        <ThumbDownOffAltIcon onClick={() => handleChangeVote(VOTE_STATE_TYPE.DISLIKE)} sx={{ fontSize: '40px', cursor: 'pointer' }} />}
+                    <Typography style={{ fontWeight: 'bold', marginRight: '20px' }}>{likes}</Typography>
+                    {vote === VOTE_STATE_TYPE.LIKE ?
+                        <ThumbUpIcon onClick={() => handleChangeVote(VOTE_STATE_TYPE.NEUTRAL)} sx={{ fontSize: '40px', cursor: 'pointer' }} />
+                        :
+                        <ThumbUpOffAltIcon onClick={() => handleChangeVote(VOTE_STATE_TYPE.LIKE)} sx={{ fontSize: '40px', cursor: 'pointer' }} />
+                    }
                 </Box>
             </Box>
         </Box>
